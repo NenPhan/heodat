6f310485f8df69c91c0fe68094e4e958679f03a6
NenPhan <thanhnen1360@gmail.com>
2023-10-26 14:38:30 +0700

    commit v1.2

--- lib/src/app/features/home/pages/home_page.dart
+++ lib/src/app/features/home/pages/home_page.dart
@@ -44,7 +44,7 @@ class _HomePageState extends HeoSfPageState<HomePage> {
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Text(
-                                  'Welcome, v1.1',
+                                  'Welcome, v1.2',
                                   style: textTheme(context)
                                       .titleLarge
                                       ?.copyWith(fontWeight: FontWeight.w700),
